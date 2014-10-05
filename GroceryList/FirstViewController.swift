293b2d9f51674f334694b12de59b94f3351060dd
bearclough <stephanie.fairclough@gmail.com>
2014-10-04 17:48:24 -0700

    Initial Commit

---
+++ GroceryList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GroceryList
+//
+//  Created by bearclough on 10/4/14.
+//  Copyright (c) 2014 bearclough. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
